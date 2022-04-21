d682d3411e0d8246fba6012cb9494a9556e565b6
faizalvian <faizalvian97@gmail.com>
2022-04-21 20:04:21 +0700

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const handleClick = () => setNav(!nav)
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
         <div className="px-2 flex justify-between items-center w-full h-full">
             <div className="flex items-center">
-                <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Brand</h1>
+                <h1 className='text-3xl uppercase font-bold mr-4 sm:text-4xl'>Brand.</h1>
                 <ul className='hidden md:flex'>
                     <li>Home</li>
                     <li>About</li>
