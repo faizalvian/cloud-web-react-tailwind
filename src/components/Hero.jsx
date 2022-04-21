faac05bbcb7e3bcfdd6845decc4f03f92922c03e
faizalvian <faizalvian97@gmail.com>
2022-04-21 09:30:10 +0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+    CloudUploadIcon,
+    DatabaseIcon,
+    PaperAirplaneIcon,
+    ServerIcon,
+} from '@heroicons/react/solid'
+import bgImg from '../assets/cyber-bg.png';
+
+const Hero = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
+        <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+            <div className="">
+                <p>Unique Sequencing & Production</p>
+                <h1>Cloud Management</h1>
+                <p>This is our Tech Brand</p>
+                <button>Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
